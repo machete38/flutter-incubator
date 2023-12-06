6ea0cc46e4b497d4e3e52546557785891ab4446f
Andrey Gareev <55807208+machete38@users.noreply.github.com>
2023-12-06 13:40:53 +0300

    Update task_1.dart

--- 1_dart_concepts/1_1_mixins/task_1.dart
+++ 1_dart_concepts/1_1_mixins/task_1.dart
@@ -1,4 +1,12 @@
 void main() {
   // Implement an extension on [DateTime], returning a [String] in format of
   // `YYYY.MM.DD hh:mm:ss` (e.g. `2023.01.01 00:00:00`).
+  DateTime date = DateTime.now();
+  print(date.dateToString());
+}
+
+extension StringOut on DateTime {
+  String dateToString() {
+    return "${year.toString()}.${month.toString().padLeft(2, '0')}.${day.toString().padLeft(2, '0')} ${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}:${second.toString().padLeft(2, '0')}";
+  }
 }
