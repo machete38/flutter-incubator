862670e4b0249245bb6ebae0aaa5ed4cbd65c4fa
Andrey Gareev <55807208+machete38@users.noreply.github.com>
2023-12-06 16:45:33 +0300

    Update task_2.dart

--- 1_dart_concepts/1_1_mixins/task_2.dart
+++ 1_dart_concepts/1_1_mixins/task_2.dart
@@ -4,4 +4,19 @@ void main() {
   // Extension should return a [List] of texts and links, e.g.:
   // - `Hello, google.com, yay` ->
   //   [Text('Hello, '), Link('google.com'), Text(', yay')].
+  print('Hello, google.com, yay'.toList());
+}
+
+extension ListParser on String {
+  List toList() {
+    List list = List.from([]);
+    split(', ').forEach((match) {
+      if (match.contains('.')) {
+        list.add('Link(\'$match\')');
+      } else {
+        list.add('Text(\'$match\')');
+      }
+    });
+    return list;
+  }
 }
